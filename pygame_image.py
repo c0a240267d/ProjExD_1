40a2ef6cf770cb47ca7caf20982c09b89a339676
shunki <c0a240267d@edu.teu.ac.jp>
2025-04-15 16:43:19 +0900

    練習10

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,30 @@ def main():
     bg_img2 = pg.transform.flip(bg_img,True,False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
+    kk_rct = kk_img.get_rect()#ren10-1
+    kk_rct.center = (300,200)#ren10-2
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr
+        key_lst = pg.key.get_pressed()#ren10-3
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))#ren10-4
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))#ren10-4
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))#ren10-4
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))#ren10-4
+
+
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, kk_rct)#renn4/10-5
         pg.display.update()
         tmr += 1        
         clock.tick(200)
